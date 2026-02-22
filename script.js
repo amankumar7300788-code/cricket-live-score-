de35194343a42fbcf08fc8e3e9c46790303083cb
Buisness <amankumar7300788@gmail.com>
2026-02-22 22:45:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,38 +2,21 @@ const API_KEY = "1592f85f-8617-4497-9fe3-16a84e459413";
 
 async function loadMatches(){
 
-let response = await fetch(
+let res = await fetch(
 `https://api.cricapi.com/v1/currentMatches?apikey=${API_KEY}&offset=0`
 );
 
-let data = await response.json();
+let data = await res.json();
 
-let liveHTML = "";
-let upcomingHTML = "";
-let recentHTML = "";
+let html = "";
 
 data.data.forEach(match => {
 
-let score = "Score not available";
-
-if(match.score.length > 0){
-
-score =
-match.score[0].r + "/" +
-match.score[0].w + " (" +
-match.score[0].o + ")";
-
-}
-
-let html = `
-<div class="matchCard">
+html += `
+<div class="matchCard" onclick="openMatch('${match.id}')">
 
 <div class="matchTitle">
 ${match.name}
-</div>
-
-<div class="matchScore">
-${score}
 </div>
 
 <div>
@@ -43,28 +26,17 @@ ${match.status}
 </div>
 `;
 
-if(match.matchStarted && !match.matchEnded){
+});
 
-liveHTML += html;
-
-}else if(!match.matchStarted){
-
-upcomingHTML += html;
-
-}else{
-
-recentHTML += html;
+document.getElementById("matchList").innerHTML = html;
 
 }
 
-});
+function openMatch(id){
 
-document.getElementById("liveMatches").innerHTML = liveHTML;
-document.getElementById("upcomingMatches").innerHTML = upcomingHTML;
-document.getElementById("recentMatches").innerHTML = recentHTML;
+window.location.href = "match.html?id=" + id;
 
 }
 
 loadMatches();
-
-setInterval(loadMatches,5000);
+setInterval(loadMatches, 10000);
